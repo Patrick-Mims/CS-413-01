c12ed95db99f165344d4439b11b9f74d40f0abc2
weatherpub <patrick@bayareamedia.net>
2023-03-09 19:45:24 -0800

    Latest

--- Project-1/src/main/java/edu/sfsu/classes/Books.java
+++ Project-1/src/main/java/edu/sfsu/classes/Books.java
@@ -2,5 +2,43 @@ package edu.sfsu.classes;
 
 import edu.sfsu.interfaces.Book;
 
-public class Books implements Book {
+public class Books {
+    private String author;
+    private String condition;
+    private String title;
+    private int published;
+    public Books() {
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublished() {
+        return published;
+    }
+
+    public void setPublished(int published) {
+        this.published = published;
+    }
 }
